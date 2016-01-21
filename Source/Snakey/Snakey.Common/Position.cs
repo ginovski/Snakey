30a96032917df302b31fe2ade390ed5c06fd8bef
Alexandar Ginovski <sashko.ginovski@gmail.com>
2016-01-21 09:49:47 +0200

    Initial commit. Created Food

---
+++ Source/Snakey/Snakey.Common/Position.cs
@@ -0,0 +1,50 @@
+﻿namespace Snakey.Common
+{
+    using System;
+
+    public class Position
+    {
+        private int x;
+        private int y;
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("X cannot be less than zero.");
+                }
+
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Y cannot be less than zero.");
+                }
+
+                this.y = value;
+            }
+        }
+    }
+}
---
+++ Source/Snakey/Snakey.Logic/GameObjects/Food.cs
@@ -0,0 +1,16 @@
+﻿namespace Snakey.Logic.GameObjects
+{
+    using Common;
+
+    public class Food
+    {
+        private const char SYMBOL = 'O';
+        
+        public Food(Position position)
+        {
+            this.Position = position;
+        }
+
+        public Position Position { get; set; }
+    }
+}
